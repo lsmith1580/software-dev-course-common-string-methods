143d78b1ba62151629c8336f7ca686d033bcd2e7
lsmith1580 <lindseysmith1580@gmail.com>
2025-03-24 13:37:21 -0500

    Add files via upload

--- common-string-methods.js
+++ common-string-methods.js
@@ -69,23 +69,28 @@ Complete the following tasks and assign the results to the specified variables. 
 let inputString = "  Welcome to the Coding Bootcamp! Learn JavaScript today.  ";
 
 // 1. Searching
-let hasJavaScript; // Your code here
-let codingPosition; // Your code here
-let startsWithWelcome; // Your code here
-let endsWithToday; // Your code here
+let hasJavaScript = console.log(inputString.includes("JavaScript")); 
+let codingPosition = console.log(inputString.indexOf("Coding"));
+let startsWithWelcome = console.log(inputString.startsWith("Welcome"));
+let endsWithToday = console.log(inputString.endsWith("today"));
 
 // 2. Transforming
-let lowercaseString; // Your code here
-let uppercaseString; // Your code here
-let trimmedString; // Your code here
-let replacedString; // Your code here
+let lowercaseString = inputString.toLowerCase();
+console.log(lowercaseString);
+let uppercaseString = inputString.toUpperCase();
+console.log(uppercaseString);
+let trimmedString = inputString.trim();
+console.log(trimmedString);
+let replacedString = inputString.replace("Javascript", "coding");
 
 // 3. Breaking Apart
-let wordsArray; // Your code here
+let wordsArray = inputString.split(" ");
 
 // 4. Retrieving
-let firstCharacter; // Your code here
-let extractedBootcamp; // Your code here
+let firstCharacter = trimmedString.charAt(0);
+console.log(firstCharacter);
+let extractedBootcamp = inputString.slice(23,32);
+console.log(extractedBootcamp);
 
 // Log all results
 console.log({
